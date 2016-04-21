77a031b6cc19ecfabd052da6795f7671d2668fd2
davidID <davidisaacdill@gmail.com>
2016-04-21 10:26:22 -0700

    Upgraded News Website With Menu Button
    This is the News Website with a Menu Button

---
+++ script.js
@@ -0,0 +1,5 @@
+/*When the user hovers over the Menu button,
+the dropdown toggles between showing and hiding*/
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show")
+}
